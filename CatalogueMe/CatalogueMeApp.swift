d31da762a2856a6cb548ee6aeb30e40f8fcacad9
pranavsuri43 <pranav.suri@uwaterloo.ca>
2023-11-16 03:40:47 -0500

    Initial Commit

---
+++ CatalogueMe/CatalogueMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatalogueMeApp.swift
+//  CatalogueMe
+//
+//  Created by Pranav Suri on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CatalogueMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
